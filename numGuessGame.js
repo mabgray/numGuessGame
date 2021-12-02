005953a92b8001dbd5775e103f69de7c08df172f
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2021-12-02 15:17:21 -0600

    fixed stuff

--- numGuessGame.js
+++ numGuessGame.js
@@ -6,12 +6,33 @@ function runGame(){
     let numTries = 0;
 
     do{
-       
-    }while (!correct) {
-        guessString = parseInt(prompt("pick a number between 1 - 100"));
-  
+        guessString = prompt("pick a number between 1 - 100");
         numTries++;
         guessNumber = +guessString;
+        console.log(guessNumber);
         correct = checkGuess(guessNumber, target);
+
+    }while (!correct) 
+
+       
+    alert("you have guessed correctly" + " it took you " + numTries);
+}
+
+function checkGuess(guessNumber, target){
+    let correct = false;
+
+    if(isNaN(guessNumber)){
+        alert("you did not enter a number");
+    } else if(guessNumber > 100 && guessNumber < 1) {
+        alert("your number is out of range. Please pick a number. ");
+
+    } else if (guessNumber > target){
+        alert("your guess is above the target number.");
+
+    }else if (guessNumber < target) {
+        alert("Your guess is lower than the target number.");
+    } else {
+        correct = true;
     }
+    return correct;
 }
